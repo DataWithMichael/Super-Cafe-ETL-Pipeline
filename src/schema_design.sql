ee2d16f69096908e9461d8f0c137b73717e27acb
Prajakta Bade <prajakta10909@gmail.com>
2025-07-30 16:30:34 +0100

    Save schema design updates before switching branch

--- src/schema_design.sql
+++ src/schema_design.sql
@@ -1,22 +1,22 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE TABLE Branches (
+CREATE TABLE IF NOT EXIST Branches (
     branch_id UUID PRIMARY KEY DEFAULT UUID_generate_v4(),
     branch_name TEXT NOT NULL,
     created_at DATE NOT NULL DEFAULT CURRENT_DATE,
 );
 
-CREATE TABLE Customers(
+CREATE TABLE IF NOT EXIST Customers(
     CUSTOMER_ID UUID PRIMARY KEY DEFAULT UUID_generate_v4()
 );
 
-CREATE TABLE Products(
+CREATE TABLE IF NOT EXIST Products(
     product_id UUID PRIMARY KEY DEFAULT UUID_generate_v4(),
     product_name TEXT NOT NULL,
     price_each DECIMAL(10, 2) NOT NULL,
     created_at DATE NOT NULL DEFAULT CURRENT_DATE
 );
 
-CREATE TABLE Orders(
+CREATE TABLE IF NOT EXIST Orders(
     order_id UUID PRIMARY KEY DEFAULT UUID_generate_v4(),
     customer_id GUID NOT NULL,
     branch_id GUID NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE Orders(
     FOREIGN KEY (product_id) REFERENCES Products(product_id)
 );
 
-CREATE TABLE Order_Items(
+CREATE TABLE IF NOT EXIST Order_Items(
     order_item_id UUID PRIMARY KEY DEFAULT UUID_generate_v4(),
     order_id GUID NOT NULL,
     product_id GUID NOT NULL,
