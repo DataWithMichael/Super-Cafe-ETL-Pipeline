c13832d015f51a5101b2a2540bfe6166ec5f5df7
Prajakta Bade <prajakta10909@gmail.com>
2025-07-30 17:14:34 +0100

    bug fix

--- src/schema_design.sql
+++ src/schema_design.sql
@@ -1,38 +1,38 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE TABLE IF NOT EXIST Branches (
+CREATE TABLE IF NOT EXISTS Branches (
     branch_id UUID PRIMARY KEY DEFAULT UUID_generate_v4(),
     branch_name TEXT NOT NULL,
-    created_at DATE NOT NULL DEFAULT CURRENT_DATE,
+    created_at DATE NOT NULL DEFAULT CURRENT_DATE
 );
 
-CREATE TABLE IF NOT EXIST Customers(
+CREATE TABLE IF NOT EXISTS Customers(
     CUSTOMER_ID UUID PRIMARY KEY DEFAULT UUID_generate_v4()
 );
 
-CREATE TABLE IF NOT EXIST Products(
+CREATE TABLE IF NOT EXISTS Products(
     product_id UUID PRIMARY KEY DEFAULT UUID_generate_v4(),
     product_name TEXT NOT NULL,
     price_each DECIMAL(10, 2) NOT NULL,
     created_at DATE NOT NULL DEFAULT CURRENT_DATE
 );
 
-CREATE TABLE IF NOT EXIST Orders(
+CREATE TABLE IF NOT EXISTS Orders(
     order_id UUID PRIMARY KEY DEFAULT UUID_generate_v4(),
-    customer_id GUID NOT NULL,
-    branch_id GUID NOT NULL,
-    product_id GUID NOT NULL,
+    customer_id UUID NOT NULL,
+    branch_id UUID NOT NULL,
+    product_id UUID NOT NULL,
     order_date DATE NOT NULL DEFAULT CURRENT_DATE,
-    total_amount DECIMAL(10, 2) NOT NULL
+    total_amount DECIMAL(10, 2) NOT NULL,
     payment_method TEXT NOT NULL,
     FOREIGN KEY (customer_id) REFERENCES Customers(CUSTOMER_ID),
-    FOREIGN KEY (branch_id) REFERENCES Branches(branch_id)
+    FOREIGN KEY (branch_id) REFERENCES Branches(branch_id),
     FOREIGN KEY (product_id) REFERENCES Products(product_id)
 );
 
-CREATE TABLE IF NOT EXIST Order_Items(
+CREATE TABLE IF NOT EXISTS Order_Items(
     order_item_id UUID PRIMARY KEY DEFAULT UUID_generate_v4(),
-    order_id GUID NOT NULL,
-    product_id GUID NOT NULL,
+    order_id UUID NOT NULL,
+    product_id UUID NOT NULL,
     quantity INT NOT NULL,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (product_id) REFERENCES Products(product_id)
