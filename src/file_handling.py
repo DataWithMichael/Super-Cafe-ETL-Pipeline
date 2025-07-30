d776de84374da797e393549627cb4f7f43136e86
mike-c7 <MichaelC@genstudents.org>
2025-07-30 14:39:18 +0100

    refactored file_handling.py to produce list of dicts

--- src/file_handling.py
+++ src/file_handling.py
@@ -1,16 +1,15 @@
 import csv
 from pathlib import Path
 
-branch_1 = Path("..") / "data" / "chesterfield_25-08-2021_09-00-00(in).csv"
-branch_2 = Path("..") / "data" / "leeds_09-05-2023_09-00-00_done(in).csv"
-branch_3 = Path("..") / "data" / "uppingham_08-08-2023_09-00-00(in).csv"
 
-
-def get_chest_data(branch_1):
+def get_chest_data():
+    branch_1 = Path("..") / "data" / "chesterfield_25-08-2021_09-00-00(in).csv"
+    branch_1_data = []
     try:
         with open(branch_1) as file:
             reader = csv.DictReader(file)
-            for branch_1_data in reader:
+            for data in reader:
+                branch_1_data.append(data)
                 return branch_1_data
 
     except FileNotFoundError as whoops:
@@ -18,11 +17,15 @@ def get_chest_data(branch_1):
     except Exception as whoops:
         print(f"An error occurred: {whoops}")
 
+
 def get_leeds_data(branch_2):
+    branch_2 = Path("..") / "data" / "leeds_09-05-2023_09-00-00_done(in).csv"
+    branch_2_data = []
     try:
         with open(branch_2) as file:
             reader = csv.DictReader(file)
-            for branch_2_data in reader:
+            for data in reader:
+                branch_2_data.append(data)
                 return branch_2_data
 
     except FileNotFoundError as whoops:
@@ -30,11 +33,15 @@ def get_leeds_data(branch_2):
     except Exception as whoops:
         print(f"An error occurred: {whoops}")
 
+
 def get_upping_data(branch_3):
+    branch_3 = Path("..") / "data" / "uppingham_08-08-2023_09-00-00(in).csv"
+    branch_3_data = []
     try:
         with open(branch_3) as file:
             reader = csv.DictReader(file)
-            for branch_3_data in reader:
+            for data in reader:
+                branch_3_data.append(data)
                 return branch_3_data
 
     except FileNotFoundError as whoops:
