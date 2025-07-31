d6a90b463e04052c9e28c5411f6a7122d664c1dc
mike-c7 <MichaelC@genstudents.org>
2025-07-31 13:18:14 +0100

    first iteration of transformation done

--- src/transformation.py
+++ src/transformation.py
@@ -1,4 +1,18 @@
-import csv 
+import csv
+import file_handling
+import os
+import uuid
+
+
+dir_path = 'C:\\Users\\micha\\OneDrive\\Documents\\GenerationDE\\ana-lattex-de-x6-generation\\data'
+count = 0
+# gets the number of files in the selected directory path (how many csv files/branches)
+for path in os.listdir(dir_path):
+    if os.path.isfile(os.path.join(dir_path, path)):
+        count += 1
+
+
+
 
 # A list of CSV files
 files = [
@@ -32,3 +46,20 @@ for filename, label in files:
         print(f"File not found: {filename}")
 
 
+test_data = ["waffle - 2.75", "burger - 5.50", "ice cream - 1.00"]
+prod_names = []
+prices_each = []
+guids = []
+
+def split_name_and_price(test_data):
+    for data in test_data:
+        
+        product = data.rsplit(" - ")
+        prod_names.append(product[0])
+        prices_each.append(product[1])
+        guid = uuid.uuid4()
+        guids.append(guid)
+
+    return prod_names, prices_each, guids
+        
+print(split_name_and_price(test_data))
